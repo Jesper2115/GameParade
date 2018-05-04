6e985d88f47db180eab6c7ce01ae2077c7fc94e5
Istrup <33019281+Istrup@users.noreply.github.com>
2018-05-04 11:10:07 +0200

    Ændringer

--- db.php
+++ db.php
@@ -1,14 +1,14 @@
 <?php
-$servername = "sql.itcn.dk:3306";
-$username = "niko5603.EADANIA";
-$password = "01u1B4DqrB";
-$database = "niko56032.EADANIA"; 
+$servername = "";
+$username = "";
+$password = "";
+$database = ""; 
 
 
 $conn = new mysqli($servername, $username, $password, $database);
 
-if ($conn->connect_error) { 
-    die("Connection epic failed" . $conn->connect_error);
-} 
-echo "Connection established :-)<br>"; 
+//if ($conn->connect_error) { 
+//    die("Connection epic failed" . $conn->connect_error);
+//} 
+//echo "Connection established :-)<br>"; 
 ?>
